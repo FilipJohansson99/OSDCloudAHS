0590c352bc79f8652d0fbaa99e799bfd64acd442
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2023-03-29 09:25:33 +0200

    Create AHSOOBE.ps1

---
+++ AHSOOBE.ps1
@@ -0,0 +1,9 @@
+$Global:oobeCloud = @{
+    oobeSetDisplay = $true
+    oobeSetRegionLanguage = $false
+    oobeSetDateTime = $true
+    oobeUpdateDrivers = $true
+    oobeUpdateWindows = $true
+    oobeRestartComputer = $true
+    oobeStopComputer = $false
+}
