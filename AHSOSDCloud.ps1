a7b8808c5ccf286452e25af138fbfd26e95af33a
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2023-04-20 10:02:56 +0200

    Update AHSOSDCloud.ps1

--- AHSOSDCloud.ps1
+++ AHSOSDCloud.ps1
@@ -2,19 +2,19 @@ Write-Host  -ForegroundColor Cyan "Starting Deployment..."
 Start-Sleep -Seconds 1
 $ErrorActionPreference = ‘SilentlyContinue’
 Write-Host  -ForegroundColor Yellow "Removing remnants on USB"
-Get-ChildItem -Path D:\OSDCloud\OS -Recurse | Remove-Item -force -recurse -Confirm:$false
+Get-ChildItem -Path D:\OSDCloud\OS -Recurse | Remove-Item -force -recurse -Confirm:$false -erroraction 'silentlycontinue'
 
 Write-Host  -ForegroundColor Yellow "D:\OSDCloud\OS Removed..."
-Get-ChildItem -Path D:\OSDCloud\DriverPacks -Recurse | Remove-Item -force -recurse -Confirm:$false
+Get-ChildItem -Path D:\OSDCloud\DriverPacks -Recurse | Remove-Item -force -recurse -Confirm:$false -erroraction 'silentlycontinue'
 
 Write-Host  -ForegroundColor Yellow "D:\OSDCloud\DriverPacks Removed..."
 
 #Update OSD
 Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-Install-Module OSD -Force
+Install-Module OSD -Force -erroraction 'silentlycontinue'
 
 Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force
+Import-Module OSD -Force -erroraction 'silentlycontinue'
 
 #Start OSDCloudGUI
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud..."
@@ -24,8 +24,8 @@ Start-OSDCloudGUI -Brand 'AHS-Deployment'
 Write-Host  -ForegroundColor Cyan "Post Actions..."
 
 Write-Host  -ForegroundColor Cyan "Cleaning up..."
-Get-ChildItem -Path C:\OSDCloud -Recurse | Remove-Item -force -recurse -Confirm:$false
-Remove-Item C:\OSDCloud -Force
+Get-ChildItem -Path C:\OSDCloud -Recurse | Remove-Item -force -recurse -Confirm:$false -erroraction 'silentlycontinue'
+Remove-Item C:\OSDCloud -Force -erroraction 'silentlycontinue'
 #Copy-Item "X:\startup.cmd" -Destination "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 #Copy-Item "X:\startup.ps1" -Destination "C:\OSDClod"
 Write-Host  -ForegroundColor Yellow "C:\OSDCloud Removed..."
