3da5d71f819ec1f03481a2285008d9b621bfd815
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2023-08-24 09:15:21 +0200

    Update AHSCloudv2.ps1

--- AHSCloudv2.ps1
+++ AHSCloudv2.ps1
@@ -17,19 +17,19 @@ Start-OSDCloudGUI -Brand 'AHS-Deployment'
 Write-Host  -ForegroundColor Cyan "Post Actions..."
 
 Write-Host  -ForegroundColor Cyan "Cleaning up..."
-Get-ChildItem -Path C:\OSDCloud -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item C:\OSDCloud -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path C:\OSDCloud -Recurse | Remove-Item -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item C:\OSDCloud -Force -Recurse -ErrorAction SilentlyContinue
 #Copy-Item "X:\startup.cmd" -Destination "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 #Copy-Item "X:\startup.ps1" -Destination "C:\OSDClod"
-Get-ChildItem -Path C:\Program Files\WindowsPowerShell\Modules\OSD -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item C:\Program Files\WindowsPowerShell\Modules\OSD -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path C:\Program Files\WindowsPowerShell\Modules\OSD -Recurse | Remove-Item -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item C:\Program Files\WindowsPowerShell\Modules\OSD -Force -Recurse -ErrorAction SilentlyContinue
 
 #Write-Host  -ForegroundColor Yellow "C:\OSDCloud Removed..."
 #Write-Host  -ForegroundColor Yellow "Removing remnants on USB..."
-#Get-ChildItem -Path D:\OSDCloud\OS -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
+#Get-ChildItem -Path D:\OSDCloud\OS -Recurse | Remove-Item -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue
 #Remove-Item D:\OSDCloud\OS -Force -ErrorAction SilentlyContinue
 #Write-Host  -ForegroundColor Yellow "D:\OSDCloud\OS Removed..."
-#Get-ChildItem -Path D:\OSDCloud\DriverPacks -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
+#Get-ChildItem -Path D:\OSDCloud\DriverPacks -Recurse | Remove-Item -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue
 #Remove-Item D:\OSDCloud\DriverPack -Force -ErrorAction SilentlyContinue
 #Write-Host  -ForegroundColor Yellow "D:\OSDCloud\DriverPacks Removed..."
 
@@ -39,8 +39,6 @@ Write-Host  -ForegroundColor Cyan "Post Actions Completed..."
 Clear-Host
 
 #Restart from WinPE
-Write-Host  -ForegroundColor Cyan "Checking Autopilot Info..."
-Get-WindowsAutopilotInfo -Online
 Write-Host  -ForegroundColor Cyan "Deployment Completed Restarting..."
 Write-Host  -ForegroundColor Yellow "PLEASE REMOVE USB AND NETWORK CONNECTION BEFORE CONTINUING"
 cmd /c pause
