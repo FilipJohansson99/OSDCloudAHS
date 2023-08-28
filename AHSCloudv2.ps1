47105452ebb6829d8a292c778837d7fe97fb2f9a
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2023-08-28 07:33:18 +0200

    Update AHSCloudv2.ps1

--- AHSCloudv2.ps1
+++ AHSCloudv2.ps1
@@ -41,9 +41,9 @@ Clear-Host
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Deployment Completed Restarting..."
 Write-Host  -ForegroundColor Yellow "REMOVE NETWORK CONNECTION BEFORE RESTARTING"
-Write-Host  -ForegroundColor Yellow "INSTALLATION WILL CONTINUE IN OOBE, DO NOT REMOVE USB"
-Write-Host  -ForegroundColor Yellow "While in OOBE reconnect internet and open cmd with SHIFT + F10. Enter the following:"
-Write-Host  -ForegroundColor Yellow "D:"
-Write-Host  -ForegroundColor Yellow "oobe.cmd"
+#Write-Host  -ForegroundColor Yellow "INSTALLATION WILL CONTINUE IN OOBE, DO NOT REMOVE USB"
+#Write-Host  -ForegroundColor Yellow "While in OOBE reconnect internet and open cmd with SHIFT + F10. Enter the following:"
+#Write-Host  -ForegroundColor Yellow "D:"
+#Write-Host  -ForegroundColor Yellow "oobe.cmd"
 cmd /c pause
 wpeutil reboot
