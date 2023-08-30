3f01f1403e77c54debd761b4b3d53eac87bd415d
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2023-08-30 08:54:04 +0200

    Depricated AHSOSDCloud.ps1

--- AHSOSDCloud.ps1
+++ AHSOSDCloud.ps1
@@ -1,43 +1,6 @@
 Write-Host  -ForegroundColor Cyan "Starting Deployment..."
 Start-Sleep -Seconds 1
 
-
-#Update OSD
-Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-Install-Module OSD -Force -ErrorAction SilentlyContinue
-
-Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force -ErrorAction SilentlyContinue
-
-#Start OSDCloudGUI
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud..."
-Start-OSDCloudGUI -Brand 'AHS-Deployment'
-
-#Post Actions
-Write-Host  -ForegroundColor Cyan "Post Actions..."
-
-Write-Host  -ForegroundColor Cyan "Cleaning up..."
-Get-ChildItem -Path C:\OSDCloud -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item C:\OSDCloud -Force -ErrorAction SilentlyContinue
-#Copy-Item "X:\startup.cmd" -Destination "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-#Copy-Item "X:\startup.ps1" -Destination "C:\OSDClod"
-Get-ChildItem -Path C:\Program Files\WindowsPowerShell\Modules\OSD -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item C:\Program Files\WindowsPowerShell\Modules\OSD -Force -ErrorAction SilentlyContinue
-
-Write-Host  -ForegroundColor Yellow "C:\OSDCloud Removed..."
-Write-Host  -ForegroundColor Yellow "Removing remnants on USB..."
-Get-ChildItem -Path D:\OSDCloud\OS -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item D:\OSDCloud\OS -Force -ErrorAction SilentlyContinue
-Write-Host  -ForegroundColor Yellow "D:\OSDCloud\OS Removed..."
-Get-ChildItem -Path D:\OSDCloud\DriverPacks -Recurse | Remove-Item -force -recurse -Confirm:$false -ErrorAction SilentlyContinue
-Remove-Item D:\OSDCloud\DriverPack -Force -ErrorAction SilentlyContinue
-Write-Host  -ForegroundColor Yellow "D:\OSDCloud\DriverPacks Removed..."
-
-
-Write-Host  -ForegroundColor Cyan "Post Actions Completed..."
-
-#Restart from WinPE
-Write-Host  -ForegroundColor Cyan "Deployment Completed Restarting..."
-Write-Host  -ForegroundColor Yellow "PLEASE REMOVE USB AND NETWORK CONNECTION BEFORE CONTINUING"
+Write-Host  -ForegroundColor Yellow "THIS USB IS DEPRICATED, CONTACT ADMINISTRATOR"
 cmd /c pause
 wpeutil reboot
